132b5a06595f3ffd12e6c0f43ce4a1c6d3ccf56b
Michael Zangl <michael.zangl@student.kit.edu>
2014-07-30 12:09:57 +0200

    Marked 0.2.1

--- Minebot/src/net/famzangl/minecraft/minebot/MinebotMod.java
+++ Minebot/src/net/famzangl/minecraft/minebot/MinebotMod.java
@@ -7,7 +7,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "minebot-mod", name = "Minebot", version = "0.2.0")
+@Mod(modid = "minebot-mod", name = "Minebot", version = "0.2.1")
 public class MinebotMod {
 	@Instance(value = "minebot-mod")
 	public static MinebotMod instance;
