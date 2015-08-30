053a24cd6b7b3c20dace0219963446ddac07f8e7
Michael Zangl <michael.zangl@student.kit.edu>
2015-08-30 15:35:31 +0200

    Added a compile script

---
+++ release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Short script to create a release.
+
+base="$(dirname "$(realpath "$0")")"
+cd "$base"
+
+tag=`git describe --exact-match --tags HEAD`
+
+if ! [ $? -eq 0 ]; then
+	echo "You should tag the current git version. Use git tag v1.2.3"
+	tag=`git rev-parse --short HEAD`
+fi
+
+echo "Using version tag $tag"
+
+mkdir -p releases/$tag || exit 1
+
+for mod in Minebot AimBow; do
+	echo "Starting to compile $mod ..."
+	cd "$base/$mod"
+	./gradlew build || exit 1
+	cp "build/libs/$(ls -t1 build/libs/ | tail -n 1)" "$base/releases/$tag" || exit 1
+done
+
+echo "Packing release"
+cd "$base/releases"
+zip $tag $tag/*
+
+
