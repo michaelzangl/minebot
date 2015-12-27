2888a5664a4ec634bc515d23b818d573ded3b38d
Michael Zangl <michael.zangl@student.kit.edu>
2015-12-27 22:33:50 +0100

    Added ChatMessage class.

---
+++ Minebot/src/net/famzangl/minecraft/minebot/ai/scripting/ChatMessage.java
@@ -0,0 +1,30 @@
+package net.famzangl.minecraft.minebot.ai.scripting;
+
+import java.util.Date;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import net.famzangl.minecraft.minebot.ai.net.MinebotNetHandler.PersistentChat;
+
+public class ChatMessage {
+
+	public final Object time;
+	public final String text;
+	public final String textFormatted;
+	public final boolean isChat;
+
+	public ChatMessage(PersistentChat m, ScriptEngine engine)
+			throws ScriptException {
+		time = engine.eval("new Date(" + m.getTime() + ")");
+		text = m.getMessage().getUnformattedText();
+		textFormatted = m.getMessage().getFormattedText();
+		isChat = m.isChat();
+	}
+
+	@Override
+	public String toString() {
+		return "ChatMessage [text=" + text + "]";
+	}
+	
+}
