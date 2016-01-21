6a15a30f6adde80f6b882bdac1682eda7d8519d7
Michael Zangl <michael.zangl@student.kit.edu>
2016-01-21 14:45:36 +0100

    Make /minebot settings edit work even if Java does not find an editor.

--- Minebot/src/net/famzangl/minecraft/minebot/ai/commands/CommandSettings.java
+++ Minebot/src/net/famzangl/minecraft/minebot/ai/commands/CommandSettings.java
@@ -5,6 +5,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+
 import net.famzangl.minecraft.minebot.ai.AIHelper;
 import net.famzangl.minecraft.minebot.ai.command.AIChatController;
 import net.famzangl.minecraft.minebot.ai.command.AICommand;
@@ -17,6 +22,9 @@ import net.famzangl.minecraft.minebot.settings.MinebotSettings;
 
 @AICommand(helpText = "Edit settings.", name = "minebot")
 public class CommandSettings {
+	private static final Marker MARKER_EDIT = MarkerManager
+			.getMarker("edit");
+	private static final Logger LOGGER = LogManager.getLogger(CommandSettings.class);
 
 	@AICommandInvocation()
 	public static AIStrategy run(
@@ -46,6 +54,7 @@ public class CommandSettings {
 					edit(MinebotSettings.getSettingsFile());
 				} catch (IOException e) {
 					AIChatController.addChatLine("Could not open the file.");
+					LOGGER.catching(e);
 				} catch (UnsupportedOperationException e) {
 					AIChatController
 							.addChatLine("Could not open an editor. Do it yourself: "
@@ -55,20 +64,53 @@ public class CommandSettings {
 			}
 
 			private void edit(File settingsFile) throws IOException {
-				try {
-					Desktop.getDesktop().edit(settingsFile);
+				if (openInJavaDefaultEditor(settingsFile)) {
 					return;
-				} catch (UnsupportedOperationException e) {
 				}
 
 				ArrayList<String> editors = new ArrayList<String>();
 
 				// TODO: Only linux
-				editors.add("edit");
+				editors.add("edit_json");
+				editors.add("gedit");
+				editors.add("kedit");
+				editors.add("gvim");
+				editors.add("notepad++.exe");
 				for (String e : editors) {
-					Runtime.getRuntime().exec(
-							new String[] { e, settingsFile.getAbsolutePath() });
+					if (openWithProgram(e, settingsFile.getAbsolutePath())) {
+						return;
+					}
 				}
+				LOGGER.info("No editor found. Attempted: " + editors);
+				throw new UnsupportedOperationException("No editor found.");
+			}
+
+			private boolean openInJavaDefaultEditor(File settingsFile)
+					throws IOException {
+				try {
+					Desktop.getDesktop().edit(settingsFile);
+					return true;
+				} catch (UnsupportedOperationException e) {
+					return false;
+				}
+			}
+
+			private boolean openWithProgram(String e, String file)
+					throws IOException {
+				String envPath = System.getenv("PATH");
+				LOGGER.trace("Open with " + e + " in " + envPath);
+				for (String dir : envPath.split("\\" + File.pathSeparator)) {
+					File testPorgram = new File(new File(dir), e);
+					LOGGER.trace("Attempt to open the file with " + testPorgram.getAbsolutePath());
+					if (testPorgram.canExecute()) {
+						Process evaluated = Runtime.getRuntime().exec(
+								new String[] { testPorgram.getAbsolutePath(), file });
+						LOGGER.trace("Success.");
+						return true;
+					}
+				}
+
+				return false;
 			}
 		};
 	}
